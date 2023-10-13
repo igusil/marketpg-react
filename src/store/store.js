aa969a7bec20c7f3c39991d7f6d031dcb0d1a74e
igusil <igusil@outlook.com>
2023-10-12 23:18:04 -0300

    new f

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,7 @@
 import { legacy_createStore as createStore, applyMiddleware} from 'redux'
 import ReduxThunk from 'redux-thunk'
 import rootReducer from './reducers/rootReducers'
-
+/* */
 let middleware = [ReduxThunk]
 
 export const store = createStore(rootReducer, applyMiddleware(...middleware))
